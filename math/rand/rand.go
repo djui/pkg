291465e46c655bdf52cc7cb47419f4b166ba748f
Uwe Dauernheim <uwe@dauernheim.net>
2016-06-29 06:53:34 +0200

    Add int and string slice permutations

--- math/rand/rand.go
+++ math/rand/rand.go
@@ -10,3 +10,39 @@ import (
 func Duration(d time.Duration) time.Duration {
 	return time.Duration(rand.Int63n(int64(d)))
 }
+
+// PermInt permutates a given slice of integers and returns a new slice.
+func PermInt(x []int) []int {
+	y := make([]int, len(x))
+	perm := rand.Perm(len(x))
+	for i, v := range perm {
+		y[v] = x[i]
+	}
+	return y
+}
+
+// PermIntInplace permutates a given slice of integers in-place.
+func PermIntInplace(x []int) {
+	for i := range x {
+		j := rand.Intn(i + 1)
+		x[i], x[j] = x[j], x[i]
+	}
+}
+
+// PermString permutates a given slice of integers and returns a new slice.
+func PermString(x []string) []string {
+	y := make([]string, len(x))
+	perm := rand.Perm(len(x))
+	for i, v := range perm {
+		y[v] = x[i]
+	}
+	return y
+}
+
+// PermStringInplace permutates a given slice of integers in-place.
+func PermStringInplace(x []string) {
+	for i := range x {
+		j := rand.Intn(i + 1)
+		x[i], x[j] = x[j], x[i]
+	}
+}
