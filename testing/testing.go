d50c27410c0b4dc845924d5406596e426df33a30
Uwe Dauernheim <uwe@dauernheim.net>
2016-07-13 20:36:05 +0200

    Add filename and line to assert output

--- testing/testing.go
+++ testing/testing.go
@@ -1,12 +1,17 @@
 package testing
 
-import "testing"
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+)
 
 // AssertEqual compares an expected to an actual interface type, and if not
 // equal, halts the test.
 func AssertEqual(t *testing.T, expected, actual interface{}) {
 	if expected != actual {
-		t.Fatalf("%s != %s", expected, actual)
+		_, fn, line, _ := runtime.Caller(1)
+		t.Fatalf("%s:%d: %s != %s", filepath.Base(fn), line, expected, actual)
 	}
 }
 
@@ -14,13 +19,15 @@ func AssertEqual(t *testing.T, expected, actual interface{}) {
 // halts the test.
 func AssertEqualString(t *testing.T, expected, actual string) {
 	if expected != actual {
-		t.Fatalf("%s != %s", expected, actual)
+		_, fn, line, _ := runtime.Caller(1)
+		t.Fatalf("%s:%d: %s != %s", filepath.Base(fn), line, expected, actual)
 	}
 }
 
 // AssertNoError compares an actual error to nil, and if nil, halts the test.
 func AssertNoError(t *testing.T, err error) {
 	if err != nil {
-		t.Fatalf("nil != %v", err)
+		_, fn, line, _ := runtime.Caller(1)
+		t.Fatalf("%s:%d: nil != %v", filepath.Base(fn), line, err)
 	}
 }
