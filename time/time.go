4b5542160ef7f4e9bdcf11e1b8a0d3e106f4762c
Uwe Dauernheim <uwe@dauernheim.net>
2016-08-15 13:08:58 +0200

    Implement JSON, Binary, and Text encoding interfaces

--- time/time.go
+++ time/time.go
@@ -1,6 +1,7 @@
 package time
 
 import (
+	"encoding/json"
 	"encoding/xml"
 	"time"
 )
@@ -10,6 +11,18 @@ type RFC3339Time struct {
 	time.Time
 }
 
+// MarshalJSON satisfies the json.Marshaler interface.
+func (c *RFC3339Time) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.Time.Format(time.RFC3339))
+}
+
+// UnmarshalJSON satisfies the json.Unmarshaler interface.
+func (c *RFC3339Time) UnmarshalJSON(json []byte) error {
+	parse, err := time.Parse(time.RFC3339, string(json))
+	if err != nil {
+		return err
+	}
+	c.Time = parse
 	return nil
 }
 
@@ -44,6 +57,37 @@ func (c *RFC3339Time) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error
 // UnmarshalXMLAttr satisfies the xml.UnmarshalerAttr interface.
 func (c *RFC3339Time) UnmarshalXMLAttr(attr xml.Attr) error {
 	parse, err := time.Parse(time.RFC3339, attr.Value)
+	if err != nil {
+		return err
+	}
+	c.Time = parse
+	return nil
+}
+
+// MarshalBinary satisfies the encoding.BinaryMarshaler interface.
+func (c *RFC3339Time) MarshalBinary() ([]byte, error) {
+	return []byte(c.Time.Format(time.RFC3339)), nil
+}
+
+// UnmarshalBinary satisfies the encoding.BinaryUnmarshaler interface.
+func (c *RFC3339Time) UnmarshalBinary(data []byte) error {
+	parse, err := time.Parse(time.RFC3339, string(data))
+	if err != nil {
+		return err
+	}
+	c.Time = parse
+	return nil
+
+}
+
+// MarshalText satisfies the encoding.TextMarshaler interface.
+func (c *RFC3339Time) MarshalText() ([]byte, error) {
+	return []byte(c.Time.Format(time.RFC3339)), nil
+}
+
+// UnmarshalText satisfies the encoding.TextUnmarshaler interface.
+func (c *RFC3339Time) UnmarshalText(text []byte) error {
+	parse, err := time.Parse(time.RFC3339, string(text))
 	if err != nil {
 		return err
 	}
